74708cbcf227533339b79b6a59d2716fca215ddf
4nth0ny1 <acatullo4@gmail.com>
2024-01-25 20:56:24 -0600

    drudgework setup windows

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,32 @@
+#include <windows.h>
+
+int CALLBACK WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nShowCmd
+)
+{
+	MessageBox(0, "its working", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+	return(0);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
