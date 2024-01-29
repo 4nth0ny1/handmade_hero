0f6747c2feb822eaa6744534857f0168a2f144eb
4nth0ny1 <acatullo4@gmail.com>
2024-01-28 21:02:24 -0600

    added red background to the window

--- code/build.bat
+++ code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi C:\Users\Anthony\Documents\code\handmade_hero\code\win32_handmade.cpp user32.lib Gdi32.lib
+cl -FC -Zi C:\Users\Anthony\Documents\code\handmade_hero\code\win32_handmade.cpp user32.lib Gdi32.lib
 popd
